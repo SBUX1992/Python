38dc0071f08798e3e598a04035452ccc9e5ab453
SBUX <sinsunkangg@naver.com>
2023-01-03 12:17:33 +0900

    2022-01-03 수업예제

---
+++ Backjoon/정답.py
@@ -0,0 +1,21 @@
+a, b = map(int, input().split())
+print(a/b)
+
+
+# 6번
+a, b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+# 7번
+print(input()+'??!')
+
+# 8번
+print(int(input())-543)
+
+
+
+
